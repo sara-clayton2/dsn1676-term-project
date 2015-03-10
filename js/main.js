16aec356e41764f749d01043294bc87dd3614994
sara clayton <clay0037@algonquinlive.com>
2015-03-10 19:59:14 -0400

    added a button that when clicked can view art work

--- js/main.js
+++ js/main.js
@@ -16,4 +16,16 @@ $btnPanel.on('click', function () {
 $pop.toggleClass('js-panel-down');
 }); 
 
-	
+var $btnShowhide = $('.btn-show-hide'); 
+var $box = $('.box'); 
+
+$btnShowhide.on('click', function () { 
+$box.toggleClass('js-box-hide');
+}); 
+
+var $btnShowhide2 = $('.btn-show-hide2'); 
+var $box2 = $('.box2'); 
+
+$btnShowhide2.on('click', function () { 
+$box2.toggleClass('js-box-hide2');
+}); 
